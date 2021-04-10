aa5f9d8dc5c362f19ef8ddded95d81d8bcf1662d
jcv <ausp.jcv@gmail.com>
2021-04-10 11:52:34 +0200

    Scene Controller added

---
+++ Robot/Assets/Scripts/SceneController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField]
+    private int endSceneIndex;
+
+    [SerializeField]
+    private int startSceneIndex;
+
+    public int CurrentSceneIndex
+    {
+        get
+        {
+            return SceneManager.GetActiveScene().buildIndex;
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadSceneAsync(CurrentSceneIndex + 1);
+        SceneManager.UnloadSceneAsync(CurrentSceneIndex);
+    }
+
+    public void LoadEndScreen()
+    {
+
+    }
+
+}
