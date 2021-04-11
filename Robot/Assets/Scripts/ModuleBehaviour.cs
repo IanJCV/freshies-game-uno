7324a4a2018ad4f58e4f14e82b3ba9d2073e55ac
jcv <ausp.jcv@gmail.com>
2021-04-11 14:24:30 +0200

    njkbgfnjkvgfdnujikbfdnj

--- Robot/Assets/Scripts/ModuleBehaviour.cs
+++ Robot/Assets/Scripts/ModuleBehaviour.cs
@@ -5,18 +5,82 @@ using UnityEngine;
 public abstract class ModuleBehaviour : ScriptableObject
 {
     public Sprite sprite;
-    public int _maxDurability;
+
     public GameObject parentObject;
+
+    public bool useDurability;
+
+    private int _durability;
+    public int maxDurability;
+
+    public int Durability
+    {
+        get
+        {
+            return _durability;
+        }
+
+        set
+        {
+            _durability = value;
+            if (_durability < 0 && useDurability)
+            {
+                _durability = 0;
+                OnBreak();
+            }
+        }
+    }
+
+    public bool useHeat;
+
+    private int _heat;
+    public int maxHeat;
+
+    public int Heat
+    {
+        get
+        {
+            return _heat;
+        }
+
+        set
+        {
+            _heat = value;
+            if (useHeat)
+            {
+                if (_heat < 0)
+                {
+                    _heat = 0;
+                }
+
+                if (_heat >= maxHeat)
+                {
+                    _heat = 0;
+                    OnOverheat();
+                }
+            }
+            else
+            {
+                _heat = 0;
+            }
+        }
+    }
+
 
     public ModuleType type;
 
     public AudioClip sound;
     public AudioSource source;
 
+    public abstract void OnInitialize();
+
     public abstract void OnPress();
     public abstract void OnRelease();
     public abstract void OnHold();
     public abstract void Update();
+
+    public abstract void OnOverheat();
+    public abstract void OnBreak();
 }
 
 public enum ModuleType
---
+++ Robot/Assets/Scripts/RocketModule.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Modules/Rocket Launcher")]
+public class RocketModule : ModuleBehaviour
+{
+    public GameObject rocketPrefab;
+
+    private Vector3 mousePosition;
+
+    public override void Update()
+    {
+
+        Vector3 vector = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+        mousePosition = Camera.main.ScreenToWorldPoint(vector);
+        mousePosition.z = 0f;
+
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        parentObject.transform.LookAt(mousePosition, Vector3.up);
+    }
+
+    public override void OnHold()
+    {
+
+    }
+
+    public override void OnPress()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        CinemachineController.Instance.ShakeCamera(5f, 0.2f);
+        Instantiate(rocketPrefab, parentObject.transform.position, parentObject.transform.rotation * Quaternion.Euler(0, -90, 0));
+        source.Play();
+    }
+
+    public override void OnRelease()
+    {
+
+    }
+
+    public override void OnBreak()
+    {
+
+    }
+
+    public override void OnOverheat()
+    {
+
+    }
+
+    public override void OnInitialize()
+    {
+
+    }
+}
